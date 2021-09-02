6e55837069713aa9686a5c1f4565a9eb3ae51cb7
itonyli <429284840@qq.com>
2021-09-03 00:20:55 +0800

    增加部分可读注释

--- journalkeeper-persistence-local/src/main/java/io/journalkeeper/persistence/local/cache/MemoryCacheManager.java
+++ journalkeeper-persistence-local/src/main/java/io/journalkeeper/persistence/local/cache/MemoryCacheManager.java
@@ -1,7 +1,5 @@
 package io.journalkeeper.persistence.local.cache;
 
-import io.journalkeeper.utils.spi.Singleton;
-
 import java.io.Closeable;
 import java.nio.ByteBuffer;
 import java.util.Collection;
@@ -12,28 +10,79 @@ import java.util.Collection;
  */
 public interface MemoryCacheManager extends Closeable {
 
-
+    /**
+     * 打印内存缓存指标详情
+     */
     void printMetric();
 
+    /**
+     * 增加一个PreLoad实体
+     * @param bufferSize buffer size
+     * @param coreCount ??
+     * @param maxCount ??
+     */
     void addPreLoad(int bufferSize, int coreCount, int maxCount);
 
+    /**
+     * 移除一个PreLoad实体
+     * @param bufferSize
+     */
     void removePreLoad(int bufferSize);
 
+    /**
+     * 分配一个MMAP内存Buffer
+     * @param bufferHolder
+     */
     void allocateMMap(BufferHolder bufferHolder);
 
+    /**
+     * 分配一个直接内存Buffer
+     * @param bufferSize
+     * @param bufferHolder
+     * @return
+     */
     ByteBuffer allocateDirect(int bufferSize, BufferHolder bufferHolder);
 
+    /**
+     * 释放一个直接内存Buffer
+     * @param byteBuffer
+     * @param bufferHolder
+     */
     void releaseDirect(ByteBuffer byteBuffer, BufferHolder bufferHolder);
 
+    /**
+     * 释放一个MMAP内存Buffer
+     * @param bufferHolder
+     */
     void releaseMMap(BufferHolder bufferHolder);
 
+    /**
+     * 获取PreLoad Cache数据
+     * @return
+     */
     Collection<PreloadCacheMetric> getCaches();
 
+    /**
+     * 获取最大内存大小
+     * @return
+     */
     long getMaxMemorySize();
 
+    /**
+     * 获取总共已经使用的内存大小
+     * @return
+     */
     long getTotalUsedMemorySize();
 
+    /**
+     * 获取总共已经使用的直接内存大小
+     * @return
+     */
     long getDirectUsedMemorySize();
 
+    /**
+     * 获取总共已经使用的MMAP内存大小
+     * @return
+     */
     long getMapUsedMemorySize();
 }
--- journalkeeper-persistence-local/src/main/java/io/journalkeeper/persistence/local/cache/PreloadCacheMetric.java
+++ journalkeeper-persistence-local/src/main/java/io/journalkeeper/persistence/local/cache/PreloadCacheMetric.java
@@ -5,14 +5,34 @@ package io.journalkeeper.persistence.local.cache;
  * Date: 2020/3/5
  */
 public interface PreloadCacheMetric {
+    /**
+     * Buffer 字节大小
+     * @return
+     */
     int getBufferSize();
 
+    /**
+     * Preload Cache 核心缓存条目数
+     * @return
+     */
     int getCoreCount();
 
+    /**
+     * Preload Cache 最大缓存条目数
+     * @return
+     */
     int getMaxCount();
 
+    /**
+     * Preload Cache 已经使用的缓存条目数
+     * @return
+     */
     int getUsedCount();
 
+    /**
+     * Preload Cache 已经缓存的条目数
+     * @return
+     */
     int getCachedCount();
 
 }
